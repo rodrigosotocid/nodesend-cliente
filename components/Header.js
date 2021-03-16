a7a07821d00202ab04d2841af6f0558ceab60778
Rodrigo <istikis@gmail.com>
2021-03-16 16:32:56 +0100

    Primeros pasos

---
+++ components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Header = () => {
+  return (
+    <header className="py-8 flex flex-col md:flex-row items-center justify-between">
+      <Link href="/">
+        <img src="logo.svg" alt="logo React NodeSend" className="w-64 mb-8 md:mb-0" />
+      </Link>
+
+      <div>
+        <Link href="/login">
+          <a className="bg-red-500 px-5 py-3 rounded-lg text-white font-bold uppercase mr-2">Iniciar Sesión</a>
+        </Link>
+        <Link href="/crearcuenta">
+          <a className="bg-black px-5 py-3 rounded-lg text-white font-bold uppercase">Crear Cuenta</a>
+        </Link>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
