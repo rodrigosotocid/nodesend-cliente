166ababfe8af9a7f775ef07676ea634f85b4a8bf
Rodrigo <istikis@gmail.com>
2021-03-16 17:34:01 +0100

    página, formulario y validación de CrearCuenta con Formik y Yup

--- pages/crearcuenta.js
+++ pages/crearcuenta.js
@@ -1,6 +1,26 @@
 import Layout from '../components/Layout'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
 export default function CrearCuenta() {
+
+  // Formulario y validación con Formik y Yup
+  const formik = useFormik({
+    initialValues: {
+      nombre: '',
+      email: '',
+      password: ''
+    },
+    validationSchema: Yup.object({
+      nombre: Yup.string().required('El nombre es obligatorio'),
+      email: Yup.string().email('El Email no es válido').required('El Email es obligatorio'),
+      password: Yup.string().required('El Password no puede ir vacío').min(6, 'El password debe contener al menos 6 caracteres')
+    }),
+    onSubmit: (valor) => {
+      console.log(valor);
+    }
+  });
+
   return (
     <Layout>
       <div className="md:w-4/5 xl:w-3/5 mx-auto mb-32">
@@ -10,6 +30,7 @@ export default function CrearCuenta() {
           <div className="w-full max-w-lg">
             <form
               className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
+              onSubmit={formik.handleSubmit}
             >
               <div className="mb-4">
                 <label
@@ -21,7 +42,16 @@ export default function CrearCuenta() {
                   className="shadow appeerance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   id="nombre"
                   placeholder="Nombre de Usuario"
+                  value={formik.values.nombre}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                 />
+                {formik.touched.nombre && formik.errors.nombre ? (
+                  <div className="my-2 bg-gray-200 border-l-4 border-red-500 text-red-700 p-4">
+                    <p className="font-bold">Error</p>
+                    <p>{formik.errors.nombre}</p>
+                  </div>
+                ) : null}
               </div>
               <div className="mb-4">
                 <label
@@ -33,7 +63,16 @@ export default function CrearCuenta() {
                   className="shadow appeerance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   id="email"
                   placeholder="Email de Usuario"
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                 />
+                {formik.touched.email && formik.errors.email ? (
+                  <div className="my-2 bg-gray-200 border-l-4 border-red-500 text-red-700 p-4">
+                    <p className="font-bold">Error</p>
+                    <p>{formik.errors.email}</p>
+                  </div>
+                ) : null}
               </div>
               <div className="mb-4">
                 <label
@@ -45,7 +84,16 @@ export default function CrearCuenta() {
                   className="shadow appeerance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   id="password"
                   placeholder="Password de Usuario"
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                 />
+                {formik.touched.password && formik.errors.password ? (
+                  <div className="my-2 bg-gray-200 border-l-4 border-red-500 text-red-700 p-4">
+                    <p className="font-bold">Error</p>
+                    <p>{formik.errors.password}</p>
+                  </div>
+                ) : null}
               </div>
               <input
                 type="submit"
